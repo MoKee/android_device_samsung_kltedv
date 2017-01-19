b9d8ef5521031ed9752d2f0f1897c93dd6a48d00
martincz <martincz@mokeedev.com>
2017-01-19 14:51:00 +0800

    kltedv: mokee-ify

--- lineage.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, device/samsung/kltedv/full_kltedv.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_DEVICE := kltedv
-PRODUCT_NAME := lineage_kltedv
---
+++ mokee.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/kltedv/full_kltedv.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := kltedv
+PRODUCT_NAME := mk_kltedv
